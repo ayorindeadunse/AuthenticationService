27a659432851d7866176ebe22f911eff515aa208
adunseayorinde <adunseayorinde@gmail.com>
2023-11-13 15:54:56 +0100

    added social registration to AuthService

--- AuthenticationService.Business/Services/AuthService.cs
+++ AuthenticationService.Business/Services/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Claims;
 using AuthenticationService.Business.Models;
 using AuthenticationService.Business.Models.DTOs;
 using AuthenticationService.Business.Services.Abstract;
@@ -11,21 +12,83 @@ namespace AuthenticationService.Business.Services
 	public class AuthService : IAuthService
 	{
 		private readonly IUserRepository _userRepository;
-		private readonly UserManager<User> _userManager;
 		private readonly SignInManager<User> _signInManager;
 		private readonly IJwtService _jwtService;
 
-		public AuthService(IUserRepository userRepository, UserManager<User> userManager, SignInManager<User> signInManager,
+		public AuthService(IUserRepository userRepository,SignInManager<User> signInManager,
 			IJwtService jwtService)
 		{
 			_userRepository = userRepository;
-			_userManager = userManager;
 			_signInManager = signInManager;
 			_jwtService = jwtService;
 		}
 
 		// refactor this to send a user token to user
-		public async Task<ResponseWrapper<string>> RegisterUserAsync(RegisterDTO registerDTO) //TODO: Consider adding Social login registration as well
+		public async Task<ResponseWrapper<string>> SocialRegisterAsync(string provider, string providerToken)
+		{
+			try
+			{
+				var externalLoginInfo = await _signInManager.GetExternalLoginInfoAsync();
+				if (externalLoginInfo != null)
+				{
+					//use this bit to debug the claims that come from the external login provider (twitter, facebook, etc)
+					// put a breakpoint here to get the values
+					var claims = externalLoginInfo.Principal.Claims.ToList();
+					foreach (var claim in claims)
+					{
+						// Use logger object instead
+						Console.WriteLine($"Claim Type: {claim.Type}, Claim Value: {claim.Value}");
+					}
+				}
+				
+				// retrieve possible values of  username and email.
+				var newUserData = new
+				{
+					Username = externalLoginInfo.Principal.FindFirstValue(ClaimTypes.Name),
+					Email = externalLoginInfo.Principal.FindFirstValue(ClaimTypes.Email)
+				};
+
+				var generatedPassword = GenerateSecurePassword();
+				var newUser = new User
+				{
+					UserName = newUserData.Username,
+					Email = newUserData.Email,
+					// set other properties after refactoring
+				};
+				
+				//Perform social registration logic...
+				var result = await _userRepository.CreateUserAsync(newUser, generatedPassword);
+				if (result.Succeeded)
+				{
+					await _signInManager.SignInAsync(newUser, isPersistent: false);
+					// Generate token
+					var token = _jwtService.GenerateJwtToken(newUser);
+					return new ResponseWrapper<string>
+					{
+						IsRequestSuccessful = true,
+						Data = token
+					};
+				}
+
+				return new ResponseWrapper<string>
+				{
+					// Registration failed
+					IsRequestSuccessful = false,
+					Errors = result.Errors.Select(e => e.Description).ToArray()
+				};
+			}
+			catch (Exception e)
+			{
+				//Log exception 
+				return new ResponseWrapper<string>
+				{
+					IsRequestSuccessful = false,
+					Errors = new[] { "Registration failed using your social login credentials. "}
+				};
+			}
+		}
+		
+		public async Task<ResponseWrapper<string>> RegisterUserAsync(RegisterDTO registerDTO) 
 		{
 			try
 			{
@@ -145,6 +208,10 @@ namespace AuthenticationService.Business.Services
             return token;
         }
 
+        private static string GenerateSecurePassword()
+        {
+	        return Guid.NewGuid().ToString();
+        }
     }
 }
 
